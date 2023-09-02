1565f3b6edd3790717f1c0cf3dbcc7f4764d438c
Paul Tarvydas <guitarvydas@gmail.com>
2023-09-02 08:09:17 -0400

    string uses nq

--- out.lisp
+++ out.lisp
@@ -1,21 +1,21 @@
 ./rewrite denoise.ohm --grammarname="DENOISE" drawiodenoise.rwr support.js <dev.drawio
 (mxfile
  (
-  (host "Electron")
-  (type "device")
-  (pages 2)
+  (host . "Electron")
+  (type . "device")
+  (pages . 2)
   )
  (
   (diagram
    (
-    (name "main")
-    (id "vXeUWlEQ6jjaBp5v4xXl")
+    (name . "main")
+    (id . "vXeUWlEQ6jjaBp5v4xXl")
     )
    (
     (mxGraphModel
      (
-      (dx 1022)
-      (shadow 0)
+      (dx . 1022)
+      (shadow . 0)
       )
      (
       (root
@@ -24,31 +24,31 @@
        (
         (mxCell
 	 (
-	  (id 0)
+	  (id . 0)
 	  )
 	 ()
 	 )
         (mxCell
 	 (
-	  (id 1)
-	  (parent 0)
+	  (id . 1)
+	  (parent . 0)
 	  )
 	 ()
 	 )
         (mxCell
 	 (
-	  (id "frVkl_TaBd00IkZIY_ho-7")
+	  (id . "frVkl_TaBd00IkZIY_ho-7")
 	  (style ((edgeStyle . orthogonalEdgeStyle)(rounded . 0)(orthogonalLoop . 1)(jettySize . auto)(html . 1)(exitX . 1)(exitY . 0.5)(exitDx . 0)(exitDy . 0)(exitPerimeter . 0)))
-	  (edge 1)
-	  (parent 1)
-	  (source "frVkl_TaBd00IkZIY_ho-1")
-	  (target "frVkl_TaBd00IkZIY_ho-4")
+	  (edge . 1)
+	  (parent . 1)
+	  (source . "frVkl_TaBd00IkZIY_ho-1")
+	  (target . "frVkl_TaBd00IkZIY_ho-4")
 	  )
 	 (
           (mxGeometry
 	   (
-	    (relative 1)
-	    (as "geometry")
+	    (relative . 1)
+	    (as . "geometry")
 	    )
 	   ()
 	   )
